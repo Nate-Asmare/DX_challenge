099caf18c1c42a15974c81e0f711183b6ea1b24c
Natnael Asmare Mebrate <m.t3sla@gmail.com>
2023-03-10 22:22:24 +0300

    Update task_1 solution

--- task_1.js
+++ task_1.js
@@ -1,44 +1,19 @@
-const vowels = ["a", "e", "i", "o", "u"];
-
 const longestWordInASentence = (sentence) => {
-  sentence += " ";
+  const words = sentence.replace(/[^a-zA-Z ]/g, "").split(" ");
+
   let longestWord = "";
-  let longestWordVowels = 0;
+  let numVowels = 0;
 
-  let word = "";
-  let wordVowels = 0;
-
-  for (let i = 0; i < sentence.length; i++) {
-    //ignore any character not an english letter or a space
-    const unicode = sentence[i].charCodeAt(0);
+  for (let word of words) {
+    const vowels = word.match(/[aeiou]/gi);
+    const numWordVowels = vowels ? vowels.length : 0;
 
     if (
-      (unicode >= 65 && unicode <= 90) ||
-      (unicode >= 97 && unicode <= 122) ||
-      unicode === 32
+      word.length > longestWord.length ||
+      (word.length === longestWord.length && numWordVowels > numVowels)
     ) {
-      //compare current word with the longest word
-      //if current word and longest word are equal choose based on highest number of vowels
-      if (unicode === 32) {
-        if (
-          word.length > longestWord.length ||
-          (word.length === longestWord.length && wordVowels > longestWordVowels)
-        ) {
-          longestWord = word;
-          longestWordVowels = wordVowels;
-        }
-
-        word = "";
-        wordVowels = 0;
-        continue;
-      }
-
-      //count vowels
-      if (vowels.includes(sentence[i].toLowerCase())) {
-        wordVowels++;
-      }
-      //collect characters until a space is reached (word)
-      word += sentence[i];
+      longestWord = word;
+      numVowels = numWordVowels;
     }
   }
 
